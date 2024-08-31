5f226eeafe068b2805f1f043a1cc1d78193c3f7a
Peter Gallwas <peter@husky.nz>
2024-09-01 08:57:42 +1200

    AHHHHHH

---
+++ .netlify/build/entry.mjs
@@ -0,0 +1,34 @@
+import { renderers } from './renderers.mjs';
+import { s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CvSoi7hX.mjs';
+import { manifest } from './manifest_BEop19U5.mjs';
+import { onRequest } from './_noop-middleware.mjs';
+import { createExports } from '@astrojs/netlify/ssr-function.js';
+
+const _page0 = () => import('./pages/_image.astro.mjs');
+const _page1 = () => import('./pages/404.astro.mjs');
+const _page2 = () => import('./pages/_---slug_.astro.mjs');
+
+const pageMap = new Map([
+    ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
+    ["node_modules/@astrojs/starlight/404.astro", _page1],
+    ["node_modules/@astrojs/starlight/index.astro", _page2]
+]);
+const serverIslandMap = new Map();
+
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    middleware: onRequest
+});
+const _args = {
+    "middlewareSecret": "51611d1c-1fe5-4dfa-aca5-5d6109f67881"
+};
+const _exports = createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+const _start = 'start';
+if (_start in serverEntrypointModule) {
+	serverEntrypointModule[_start](_manifest, _args);
+}
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
