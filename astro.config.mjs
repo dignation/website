2d832faa78f6028b732754882739b99f5e4fdba8
Liam Sherwin <something.random.i.made.at.3am@gmail.com>
2024-09-01 22:58:34 +1200

    Updated Members Grid

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,30 +3,30 @@ import starlight from '@astrojs/starlight';
 
 import netlify from '@astrojs/netlify';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.dignation.nz',
 
-  integrations: [
-      starlight({
-          title: 'DigNation',
-          social: {
-              github: 'https://github.com/dignation/website',
+  integrations: [starlight({
+      title: 'DigNation',
+      social: {
+          github: 'https://github.com/dignation/website',
+      },
+      sidebar: [
+          {
+              label: 'Getting started',
+              items: [
+                  // Each item here is one entry in the navigation menu.
+                  { label: 'How to join', slug: 'getting-started/how-to-join' },
+              ],
           },
-          sidebar: [
-              {
-                  label: 'Getting started',
-                  items: [
-                      // Each item here is one entry in the navigation menu.
-                      { label: 'How to join', slug: 'getting-started/how-to-join' },
-                  ],
-              },
-          ],
-          logo: {
-              src: './src/assets/logo.png',
-            },
-      }),
-	],
+      ],
+      logo: {
+          src: './src/assets/logo.png',
+        },
+  }), tailwind()],
 
   output: 'server',
   adapter: netlify(),
